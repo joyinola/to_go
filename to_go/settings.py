13413968704248c088cd261c23c95c0c499c3743
damilola <damilolaadetoro1@gmail.com>
2023-11-21 12:05:03 +0100

    change database to postgresql

--- to_go/settings.py
+++ to_go/settings.py
@@ -107,7 +107,7 @@ REST_FRAMEWORK = {
 
 DATABASES = {  
         'default': {  
-            'ENGINE': 'django.db.backends.mysql',  
+            'ENGINE': 'django.db.backends.postgresql',  
             'NAME': os.getenv('database'),  
             'USER': os.getenv('user'),  
             'PASSWORD': os.getenv('password'),  
