80cb64d60a4d3fd67bde77b2479e8cdaa08c1544
damilola <damilolaadetoro1@gmail.com>
2023-12-07 19:20:06 +0100

    update allowed host

--- to_go/settings.py
+++ to_go/settings.py
@@ -34,9 +34,11 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS =  ['.herokuapp.com','127.0.0.1:8000','localhost:8000']
+
+# ['.herokuapp.com','127.0.0.1:8000','localhost:8000']
 
 
 # Application definition
