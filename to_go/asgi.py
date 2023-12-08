0c89364012b14d12e55462a3eced456ec6a93bbb
damilola <damilolaadetoro1@gmail.com>
2023-12-08 07:10:31 +0100

    fixed heroku bug

--- to_go/asgi.py
+++ to_go/asgi.py
@@ -15,11 +15,13 @@ from channels.routing import ProtocolTypeRouter,URLRouter
 from django.core.asgi import get_asgi_application
 
 from chat.middleware.user_auth_middleware import UserAuthMiddleware
-import chat.routing
+
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "to_go.settings")
 
 django_asgi_app = get_asgi_application()
+
+import chat.routing
 
 application = ProtocolTypeRouter({
     "http": django_asgi_app,
