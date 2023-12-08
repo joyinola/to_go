e149c75ce2f4bb837d193ae21883683251ee9b24
damilola <damilolaadetoro1@gmail.com>
2023-12-08 08:15:47 +0100

    fixed heroku bug

--- Procfile
+++ Procfile
@@ -1,3 +1,3 @@
 release: python manage.py migrate
-web: gunicorn to_go.wsgi
+ web: daphne to_go.asgi:application --port $PORT --bind 0.0.0.0 -v2
 
--- to_go/asgi.py
+++ to_go/asgi.py
@@ -9,11 +9,12 @@ https://docs.djangoproject.com/en/4.2/howto/deployment/asgi/
 
 
 import os
- 
-from channels.routing import ProtocolTypeRouter,URLRouter
+
 from django.core.asgi import get_asgi_application
-from chat.middleware.user_auth_middleware import UserAuthMiddleware
 django_asgi_app = get_asgi_application()
+
+from chat.middleware.user_auth_middleware import UserAuthMiddleware
+from channels.routing import ProtocolTypeRouter,URLRouter
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "to_go.settings")
 
@@ -30,5 +31,6 @@ application = ProtocolTypeRouter({
 })
 
 # web: daphne to_go.asgi:application --port $PORT --bind 0.0.0.0 -v2
+
 # worker: python manage.py runworker channel_layer -v2
  
